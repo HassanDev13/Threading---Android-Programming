24ba883c061d6a53b5fbe1a4b3f730886fe1ba03
HassanDev13 <hassantupe1999@gmail.com>
2020-11-29 08:47:25 +0000

    Android T

---
+++ app/src/main/java/com/practice/backgroundtheardpratice/LooperMessageQueueHandlerExplained.java
@@ -0,0 +1,14 @@
+package com.practice.backgroundtheardpratice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LooperMessageQueueHandlerExplained extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_looper_message_queue_handler_explained);
+    }
+}
